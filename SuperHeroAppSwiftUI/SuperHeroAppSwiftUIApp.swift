b542878638ff07a2cb3133b6a185e237dc48fa90
Yunus <skysoft.founder@gmail.com>
2023-12-21 17:51:44 +0300

    Initial Commit

---
+++ SuperHeroAppSwiftUI/SuperHeroAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroAppSwiftUIApp.swift
+//  SuperHeroAppSwiftUI
+//
+//  Created by Yunus Can on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
